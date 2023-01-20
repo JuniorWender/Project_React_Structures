4083e89b311f86f414434058fadf46c3f7d35b35
wender.junior <wender.junior@etaure.com.br>
2023-01-20 08:51:17 -0300

    add react icons to project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
-import Home from './pages/Home'
-import Company from './pages/Company'
-import Contact from './pages/Contact'
-import Navbar from "./components/Navbar";
-
 function App() {
 
+  
   return (
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/company" element={<Company/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-      </Routes>
+    <div className="App">
 
-    </Router>
+    </div>
   );
 }
 
