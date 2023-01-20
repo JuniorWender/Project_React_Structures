3c1dc61dfd7731566b1439e4dad4d2944e87bde4
wender.junior <wender.junior@etaure.com.br>
2023-01-20 09:26:17 -0300

    add icons to footer and css style

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,14 @@
-import { FaFacebook } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
 
 function Footer(){
     return(
         <footer>
-            <FaFacebook/>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
         </footer>
     )
 
